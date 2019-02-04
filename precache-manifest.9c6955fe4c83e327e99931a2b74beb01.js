f1727163c341e3c597a17c6f4be00410bfb51bbd
Trần Thị Ngọc Trâm <eve@Eve.local>
2019-02-04 16:11:34 +0700

    Updates

---
+++ precache-manifest.9c6955fe4c83e327e99931a2b74beb01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-resume/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "54fe26f0c54799edc228",
+    "url": "/my-resume/static/js/runtime~main.54fe26f0.js"
+  },
+  {
+    "revision": "af94e220db3615da5465",
+    "url": "/my-resume/static/js/main.af94e220.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/my-resume/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "af94e220db3615da5465",
+    "url": "/my-resume/static/css/main.4da6ff6c.chunk.css"
+  },
+  {
+    "revision": "046da9e16d3c364f96f5e2bd33a7ab89",
+    "url": "/my-resume/index.html"
+  }
+];
